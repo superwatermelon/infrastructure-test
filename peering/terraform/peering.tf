9e5fcfda703c1443de4abc0a27d45297aad2ce3b
Stuart Wakefield <me@stuartwakefield.co.uk>
2017-10-08 17:00:48 +0100

    Move accepter properties to accepter resource

--- peering/terraform/peering.tf
+++ peering/terraform/peering.tf
@@ -71,10 +71,6 @@ resource "aws_vpc_peering_connection" "test" {
   peer_owner_id = "${data.aws_caller_identity.internal.account_id}"
   auto_accept   = false
 
-  accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
-
   requester {
     allow_remote_vpc_dns_resolution = true
   }
@@ -84,4 +80,8 @@ resource "aws_vpc_peering_connection_accepter" "internal" {
   provider                  = "aws.internal"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.test.id}"
   auto_accept               = true
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
 }
