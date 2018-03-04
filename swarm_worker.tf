588e02dc0a4e3162a933a5972285163aeebe2306
Stuart Wakefield <me@stuartwakefield.co.uk>
2018-03-04 20:05:46 +0000

    Ensure workers are recreated when manager is

--- swarm_worker.tf
+++ swarm_worker.tf
@@ -7,6 +7,7 @@ module "swarm_worker" {
   manager_host  = "${module.swarm_manager.private_ip}"
   key_name      = "${var.swarm_worker_key_pair}"
   instance_type = "${var.swarm_worker_instance_type}"
+  name_prefix   = "swarm-worker-${module.swarm_manager.id}-"
 
   vpc_zone_identifier = ["${aws_subnet.private.*.id}"]
 
@@ -14,4 +15,8 @@ module "swarm_worker" {
     "${aws_security_group.swarm_worker_sg.id}",
     "${aws_security_group.swarm_node_sg.id}"
   ]
+
+  tags {
+    manager = "${module.swarm_manager.instance_id}"
+  }
 }
