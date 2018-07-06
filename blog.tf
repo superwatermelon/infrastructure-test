b4c30a3120cf98155b7f183f63e3bec8380f59da
Stuart Wakefield <me@stuartwakefield.co.uk>
2018-07-06 14:34:58 +0100

    Add HTTP and HTTPS listener

--- blog.tf
+++ blog.tf
@@ -3,6 +3,7 @@ module "blog" {
   artifact_bucket_name = "${var.blog_artifact_bucket_name}"
   vpc_id = "${aws_vpc.vpc.id}"
   asg_id = "${module.swarm_worker.asg}"
-  alb_arn = "${aws_lb_listener.app_https.arn}"
+  http_listener_arn = "${aws_lb_listener.app_http.arn}"
+  https_listener_arn = "${aws_lb_listener.app_https.arn}"
   host = "blog.${var.public_hosted_zone}"
 }
